c77a8c5fc50f40a237bf880ecf310a8095721e1e
Karthik150502 <karthikrdycool@gmail.com>
2024-09-27 23:48:27 +0530

    Latest

--- test.js
+++ test.js
@@ -1,41 +1,41 @@
-const AWS = require('aws-sdk');
-async function deleteDoc(fileKey) {
-    AWS.config.update({
-        accessKeyId: "AKIAX5ZI6C2LAGGLAUDE",
-        secretAccessKey: "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",
-    })
-    try {
+// const AWS = require('aws-sdk');
+// async function deleteDoc(fileKey) {
+//     AWS.config.update({
+//         accessKeyId: "AKIAX5ZI6C2LAGGLAUDE",
+//         secretAccessKey: "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",
+//     })
+//     try {
 
-        const s3 = new AWS.S3({
-            params: {
-                Bucket: "pdf-ai-chat-karthikj"
-            },
-            region: 'eu-north-1'
-        })
-
-
-        let params = {
-            Bucket: "pdf-ai-chat-karthikj",
-            Key: fileKey,
-        };
+//         const s3 = new AWS.S3({
+//             params: {
+//                 Bucket: "pdf-ai-chat-karthikj"
+//             },
+//             region: 'eu-north-1'
+//         })
 
 
-        // let res = await s3.deleteObject(params)
-        const data = await s3.deleteObject(params).promise()
+//         let params = {
+//             Bucket: "pdf-ai-chat-karthikj",
+//             Key: fileKey,
+//         };
 
-        console.log(data)
-        // s3.deleteObject(params, (err, data) => {
-        //     if (err) {
-        //         throw new Error("Was not able to delete the object, try again later")
-        //     } else {
-        //         console.log(data)
-        //     }
-        // })
 
-    } catch (e) {
-        console.log(e)
-    }
-}
+//         // let res = await s3.deleteObject(params)
+//         const data = await s3.deleteObject(params).promise()
 
-deleteDoc("1727441179708Karthik-J-15052002.pdf");
+//         console.log(data)
+//         // s3.deleteObject(params, (err, data) => {
+//         //     if (err) {
+//         //         throw new Error("Was not able to delete the object, try again later")
+//         //     } else {
+//         //         console.log(data)
+//         //     }
+//         // })
+
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+
+// deleteDoc("1727441179708Karthik-J-15052002.pdf");
 
