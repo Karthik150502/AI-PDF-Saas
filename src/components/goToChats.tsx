14a3b3eb64da0c56763ea763436c98e5d21e6f63
Karthik150502 <karthikrdycool@gmail.com>
2024-09-28 12:30:06 +0530

    Latest

--- src/components/goToChats.tsx
+++ src/components/goToChats.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
 import { ArrowRight } from 'lucide-react'
+import { auth } from '@clerk/nextjs/server'
+import { getCachedUserChat } from '@/lib/cache'
 type Props = {
     chatId: number
 }
-export default function GoToChats({ chatId }: Props) {
+export default async function GoToChats({ chatId }: Props) {
+    const { userId } = auth();
+    let chat = await getCachedUserChat(userId)
+
+
     return (
         <Button className="flex gap-x-1 rounded-full">
-            <Link href={`/chats/${chatId}`}>Go to Chats</Link>
+            <Link href={`/chats/${chat.id}`}>Go to Chats</Link>
             <ArrowRight strokeWidth={1} />
         </Button>
     )
