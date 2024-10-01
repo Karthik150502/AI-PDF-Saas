663e3bc8998dc9ca6e0f334610a491db334d0c49
Karthik150502 <karthikrdycool@gmail.com>
2024-10-01 14:55:59 +0530

    COnfigured the AWS IAM Policy, not the document delete should work.

--- test.js
+++ test.js
@@ -1,38 +1,38 @@
-const AWS = require('aws-sdk');
-async function deleteDoc(fileKey) {
+// const AWS = require('aws-sdk');
+// async function deleteDoc(fileKey) {
 
-    AWS.config.update({
-        accessKeyId: "AKIAX5ZI6C2LAGGLAUDE",
-        secretAccessKey: "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",
-    })
-    try {
+//     AWS.config.update({
+//         accessKeyId: "AKIAX5ZI6C2LAGGLAUDE",
+//         secretAccessKey: "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",
+//     })
+//     try {
 
-        const s3 = new AWS.S3({
-            params: {
-                Bucket: "pdf-ai-chat-karthikj"
-            },
-            region: 'eu-north-1'
-        })
-
-
-        let params = {
-            Bucket: "pdf-ai-chat-karthikj",
-            Key: fileKey,
-        };
+//         const s3 = new AWS.S3({
+//             params: {
+//                 Bucket: "pdf-ai-chat-karthikj"
+//             },
+//             region: 'eu-north-1'
+//         })
 
 
-        // let res = await s3.deleteObject(params)
-        s3.deleteObject(params, (err, data) => {
-            if (err) console.log(err, err.stack); // an error occurred
-            else console.log(data);           // successful response
-        });
+//         let params = {
+//             Bucket: "pdf-ai-chat-karthikj",
+//             Key: fileKey,
+//         };
+
+
+//         // let res = await s3.deleteObject(params)
+//         s3.deleteObject(params, (err, data) => {
+//             if (err) console.log(err, err.stack); // an error occurred
+//             else console.log(data);           // successful response
+//         });
 
 
 
-    } catch (e) {
-        console.log(e)
-    }
-}
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
 
-deleteDoc("1727441179708Karthik-J-15052002.pdf");
+// deleteDoc("uploads/1727461749167Karthik-J-New-Resume-September-9th.pdf");
 
