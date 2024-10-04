f655bf18ee45584c2010d7f47ac4ec4ed00bf110
Karthik150502 <karthikrdycool@gmail.com>
2024-10-04 08:20:41 +0530

    Latest changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default async function Page() {
           </div>
           <div
 
-            className="flex mt-1">
+            className="flex mt-1 gap-x-2 gap-y-2 my-2">
             {
               isAuth && songleChat && <GoToChats chat={songleChat} />
             }
@@ -48,16 +48,14 @@ export default async function Page() {
                 <SubscriptionButton isPro={isPro} />
               </div>
             }
-          </div>
-
-
-          <p className="max-w-xl text-xs mt-1 text-slate-600">How magical would it be if your study material could talk with you, and answer all your douts? Well say no more. </p>
-          <div
-            className="w-full mt-2">
             {
-              isAuth ? <FileUpload /> : <Link href="sign-in"><Button className="rouded-none text-sm">Lets get started <LogIn strokeWidth={1} size={20} className="ml-1" /></Button></Link>
+              isAuth ? <FileUpload /> : <Link href="sign-in"><Button className="rounded-none text-sm">Lets get started <LogIn strokeWidth={1} size={20} className="ml-1" /></Button></Link>
             }
           </div>
+
+
+          <p className="max-w-xl text-xs mt-1 text-slate-600 font-bold">How magical would it be if your study material could talk with you, and answer all your douts? Well say no more. </p>
+
 
         </div>
       </div>
